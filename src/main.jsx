579cf020bb742957813a2a8458df35e1af91c4a7
Auguzcht <adNodado@mcm.edu.ph>
2025-06-27 03:41:20 +0800

    Update Routing 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Get the root element
+const rootElement = document.getElementById('root');
+
+// Create a root and render the app
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
